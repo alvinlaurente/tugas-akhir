9ad6a4bb541e4bbc2a43b321bb11891f0adf35b2
Alvin Laurent Ertanto <alvin.ertanto@gmail.com>
2021-03-10 16:42:18 +0700

    rename variable

--- test1.py
+++ test1.py
@@ -1,59 +1,58 @@
-import os
 import pyshark
-import xlsxwriter
 
-timespan1 = 0
-bits1 = 0
-throughput1 = 0
-latency_capture1 = 0
-latency_display1 = 0
-count1 = 0
-total1 = 0
+throughput = 0
+latency_capture = 0
+latency_display = 0
 
+timespan = 0
+bits = 0
+kilobits = 0
 bytes = 0
-total = 0
 count = 0
-timestamp1 = 0
-timestamp2 = 0
-timestamp3 = 0
-timestamp4 = 0
-timestamp5 = 0
-timestamp6 = 0
-timestamp7 = 0
+initial_timestamp = 0
+last_timestamp = 0
+interval = 0
+total_time = 0
+total_interval = 0
+total_actual_timestamp = 0
 
-cap = pyshark.FileCapture('eoip/10mb/1.pcapng', only_summaries=True)
-cap
+cap = pyshark.FileCapture('ipip/10mb/4.pcapng', only_summaries=True)
+# cap
 
 for pkt in cap:
-    src_addr = pkt.source
-    dst_addr = pkt.destination
-    lengths = pkt.length
-    timestamp = pkt.time
+    interval = (float(pkt.time) - float(last_timestamp))
+    last_timestamp = float(pkt.time)
 
-    timestamp4 = (float(timestamp) - float(timestamp3))
-    timestamp3 = float(timestamp)
-
-    if(src_addr == '10.0.0.114'):
-        total += int(lengths)
+    if(pkt.source == '10.0.0.114'):
+        bytes += int(pkt.length)
 
         if count == 0:
-            timestamp1 = float(timestamp)
-            timestamp6 = float(timestamp)
+            initial_timestamp = float(pkt.time)
         else:
-            timestamp2 = float(timestamp)
-            timestamp7 += timestamp3 - timestamp6
-            timestamp6 = timestamp3
+            total_time = float(pkt.time)
+            total_actual_timestamp += last_timestamp
 
         count += 1
-        timestamp5 += float(timestamp4)
-        timespan = timestamp2 - timespan1
-        bytes = total * 8
-        throughput = bytes/timespan
-        latency_capture1 = timestamp5 / count
-        latency_display1 = timestamp7 / count
+        total_interval += float(interval)
+        timespan = total_time
+        bits = bytes * 8
+        latency_capture = total_interval / count
+        latency_display = total_actual_timestamp / count
+kilobits = bits / 1000
+throughput = kilobits/timespan
 
-print("bytes", bytes)
-print("timespan", timespan)
-print("count", count)
-print("latency_capture1", latency_capture1)
-print("latency_display1", latency_display1)
+print(bits, "bits")
+print(bytes, "bytes")
+print("timespan", timespan, "s")
+print("count", count, "packets")
+print("throughput", throughput, "kb/s")
+print("latency_capture", latency_capture, "s")
+print("latency_display", latency_display, "s")
+print("")
+
+print("initial_timestamp", initial_timestamp, "s")
+print("last_timestamp", last_timestamp, "s")
+print("interval", interval, "s")
+print("total_time", total_time, "s")
+print("total_interval", total_interval, "s")
+print("total_actual_timestamp", total_actual_timestamp, "s")
