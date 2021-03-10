c397eef01b608481a1f0606ab456214b69b445fe
Alvin Laurent Ertanto <alvin.ertanto@gmail.com>
2021-03-10 16:58:48 +0700

    rev

--- test1.py
+++ test1.py
@@ -1,58 +1,61 @@
 import pyshark
 
 throughput = 0
-latency_capture = 0
+average_latency = 0
 latency_display = 0
 
 timespan = 0
 bits = 0
 kilobits = 0
 bytes = 0
-count = 0
+total_packets = 0
 initial_timestamp = 0
 last_timestamp = 0
-interval = 0
+latency = 0
 total_time = 0
-total_interval = 0
-total_actual_timestamp = 0
+total_latency = 0
+# delay = 0
 
 cap = pyshark.FileCapture('ipip/10mb/4.pcapng', only_summaries=True)
-# cap
+cap
 
 for pkt in cap:
-    interval = (float(pkt.time) - float(last_timestamp))
-    last_timestamp = float(pkt.time)
+    src_addr = pkt.source
+    lengths = pkt.length
+    current_timestamp = pkt.time
 
-    if(pkt.source == '10.0.0.114'):
-        bytes += int(pkt.length)
+    latency = (float(current_timestamp) - float(last_timestamp))
+    last_timestamp = float(current_timestamp)
 
-        if count == 0:
-            initial_timestamp = float(pkt.time)
+    if(src_addr == '10.0.0.114'):
+        bytes += int(lengths)
+
+        if total_packets == 0:
+            initial_timestamp = float(current_timestamp)
         else:
-            total_time = float(pkt.time)
-            total_actual_timestamp += last_timestamp
+            total_time = float(current_timestamp)
 
-        count += 1
-        total_interval += float(interval)
+        total_packets += 1
+        total_latency += float(latency)
         timespan = total_time
         bits = bytes * 8
-        latency_capture = total_interval / count
-        latency_display = total_actual_timestamp / count
+        average_latency = total_latency / total_packets
 kilobits = bits / 1000
 throughput = kilobits/timespan
 
 print(bits, "bits")
 print(bytes, "bytes")
 print("timespan", timespan, "s")
-print("count", count, "packets")
+print("total_packets", total_packets, "packets")
 print("throughput", throughput, "kb/s")
-print("latency_capture", latency_capture, "s")
-print("latency_display", latency_display, "s")
+print("average_latency", average_latency, "s")
+
 print("")
 
-print("initial_timestamp", initial_timestamp, "s")
-print("last_timestamp", last_timestamp, "s")
-print("interval", interval, "s")
-print("total_time", total_time, "s")
-print("total_interval", total_interval, "s")
-print("total_actual_timestamp", total_actual_timestamp, "s")
+# print("Last Packet Information")
+# print("initial_timestamp", initial_timestamp, "s")
+# print("current_timestamp", current_timestamp, "s")
+# print("last_timestamp", last_timestamp, "s")
+# print("latency", latency, "s")
+# print("total_time", total_time, "s")
+# print("total_latency", total_latency, "s")
